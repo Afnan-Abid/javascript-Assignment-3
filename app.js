11ba1fd8756aa31f8364a29b6b837e54edbb84ae
Afnan-Abid <afnanabid765@gmail.com>
2023-10-28 04:32:59 -0700

    full work

---
+++ app.js
@@ -0,0 +1,417 @@
+//4 Variables Names Legal & Illegal //
+
+
+/*Question 1
+
+
+let hello = "Hello World"
+let play = " Play Cricket"
+let wonderful = "Pakistan is wonderful country"*/
+
+
+/*Question 2
+
+let worldNames = "Pakistan"
+let yourAge = "20"
+let containerName = "Top footer"
+let yourHobbies = "Playing Cricket"
+let countryName = "England"
+let Names = "muhammad Ali"
+let Countryname = "france"
+let Age = "23"
+let Work = "salesman"
+let Hobbies = "Reading books"*/
+
+
+
+//Question 3//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Math Expression//
+
+
+/*Question 1
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber + secondNumber)*/
+
+
+
+/*Question 2
+
+
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber - secondNumber)
+
+
+
+
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber * secondNumber)
+
+
+
+
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber / secondNumber)
+
+
+
+
+
+
+const firstNumber = +prompt("Enter your first number");
+const secondNumber = +prompt("Enter your second number");
+const totalNumber = alert(firstNumber % secondNumber)*/
+
+
+
+
+//Question 3
+
+
+/*a , b
+
+let myVariable;
+alert("Value after variable declaration is: " + myVariable);*/
+
+
+/*c ,d 
+
+let number = 50
+alert("Intial value is : " + number);*/
+
+
+
+/* e , f 
+
+var number = 20;
+number = ++number;
+alert("Value after variable increment is: " + number);*/
+
+
+
+/*  g , h 
+
+
+var number = 7;
+number += 4;
+alert("Value after addition is: " + number);*/
+
+
+
+
+/*i , j 
+
+
+
+var number = 20;
+number = --number;
+alert("Value after variable decrement is: " + number);*/
+
+
+
+/*Question 4 
+
+
+var ticketPrice = 600; 
+var numberOfTickets = 5;
+var totalCost = ticketPrice * numberOfTickets;
+alert("Total cost to buy" + numberOfTickets + " tickets to a movie is " + totalCost + " PKR");*/
+
+
+/*Question 5
+
+var priceItem = 320;
+var priceItem2 = 400;
+var orderedQuantity = 10;
+var orderedQuantity2 = 5;
+
+var totalCostItem1 = priceItem * orderedQuantity;
+var totalCostItem2 = priceItem2 * orderedQuantity2;
+
+var overallTotalCost = totalCostItem1 + totalCostItem2;
+
+var receipt = "Receipt:\n";
+receipt += "Price of item 1 is" + priceItem + " PKR,  Quantity of item 1 is" + orderedQuantity + " "
+receipt += "Price of item 2 is" + priceItem2 + " PKR, Quantity of item 2 is " + orderedQuantity2 + " "
+receipt += "Total cost of your order is 2270" + overallTotalCost + " PKR";
+
+document.write(receipt);*/
+
+
+
+/*Question 6
+var studentName = prompt("Enter your name");
+var chemMarks = (prompt("Enter your Chemistry Marks"));
+var mathsMarks = (prompt("Enter your Maths Marks"));
+var phyMarks = (prompt("Enter your Physics Marks"));
+
+var totalMarksObtained = 900;
+
+var totalMarks = chemMarks + mathsMarks + phyMarks;
+var percentage = (totalMarks / totalMarksObtained) * 100;
+
+alert("Percentage: " + percentage + "%");*/
+
+
+
+/*Question 7
+
+var initialValue = 50; 
+
+var result = (((initialValue + 5) * 10) / 2);
+
+alert("Result: " + result);*/
+
+
+
+/*Question 8 
+
+
+
+
+var birthYear = parseInt(prompt("Enter your birth year"));
+var currentYear = prompt("enter your current year ")
+
+var age = currentYear - birthYear;
+
+alert("Your age is: " + age + " years");*/
+
+
+
+
+/*Question 15
+
+
+
+var snacks = prompt("Enter snacks name")
+var currentAge = prompt("Enter your current age")
+var maximumAge = prompt("Enter your maximum age")
+var amountPerDay = prompt("Enter your Amount per day")
+
+
+var yearsRemaining = maximumAge - currentAge;
+var daysRemaining = yearsRemaining * 365;
+
+var totalConsumption = amountPerDay * daysRemaining;
+
+alert("You will need  " + totalConsumption + " " + snacks + " to last you until the ripe old age ");*/
+
+
+// User input and conditional statement //
+
+
+/*Question 1
+
+
+
+var names = prompt("Enter your name")
+alert(names)*/
+
+
+
+/*Question 2 
+
+
+        var number = prompt("Enter a number for the multiplication table")
+
+        var table = "Multiplication Table for " + number + ":\n";
+        table += number + " x 1 = " + (number * 1) + "\n";
+        table += number + " x 2 = " + (number * 2) + "\n";
+        table += number + " x 3 = " + (number * 3) + "\n";
+        table += number + " x 4 = " + (number * 4) + "\n";
+        table += number + " x 5 = " + (number * 5) + "\n";
+        table += number + " x 6 = " + (number * 6) + "\n";
+        table += number + " x 7 = " + (number * 7) + "\n";
+        table += number + " x 8 = " + (number * 8) + "\n";
+        table += number + " x 9 = " + (number * 9) + "\n";
+        table += number + " x 10 = " + (number * 10) + "\n";
+
+        alert(table);*/
+
+
+/* Question 3 
+
+
+var city = prompt("Enter city name");
+
+if (city === "karachi") {
+  alert("Welcome to the city of lights, Karachi!");
+} else {
+  alert("Sorry, you are not in the city of lights, Karachi.");
+}*/
+
+
+/* Question 4 
+
+var gender = prompt("Enter your gender");
+
+if (gender === "Male") {
+  alert("Good morning Sir.");
+} else {
+  alert("Good morning maam.");
+}*/
+
+
+
+/* Question 5 
+
+
+var trafficSignal = prompt("Road Traffic Signal");
+
+if (trafficSignal === "Red") {
+  alert("Vehicles must Stop.");
+} 
+else if (trafficSignal === "Yellow") {
+  alert("Vehicles should get ready to move.");
+} else {
+  alert("Vehicles can move now.");
+}*/
+
+/*Question 6
+
+var age = prompt("Enter your max age");
+var age1 = prompt("Enter your current age");
+if (age1 <= age) {
+  alert("you are welcome");
+} 
+ else {
+  alert("you are not welcome.");
+}*/
+
+
+/*Question 7 
+
+var currentFuel = +prompt("Enter your current fuel in litres");
+var remainingFuel = +prompt("Enter your Remaining fuel");
+if (currentFuel < remainingFuel ) {
+  alert("Your fuel is full");
+} 
+ else {
+  alert("Please refill the fuel in your car");
+}*/
+
+
+
+
+
+/* Question 9
+
+
+/*const percentage = prompt("Enter your percentage");
+
+let grade;
+let remarks;
+
+if (percentage >= 80) {
+    grade = "Grade A+";
+    remarks = "Excellent";
+} else if (percentage >= 70) {
+    grade = "Grade A";
+    remarks = "Good";
+} else if (percentage >= 60) {
+    grade = "Grade B";
+    remarks = "You need to improve";
+} else {
+    grade = "Fail";
+    remarks = "Sorry";
+}
+
+alert("Grade: " + grade + "\nRemarks: " + remarks + "\nPercentage: " + percentage);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
